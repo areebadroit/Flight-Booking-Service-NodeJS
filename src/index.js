944ee6b6670bb87a53455d61336bf2118a2eae23
Md Areeb Ansar <mdareebansar@gmail.com>
2023-12-17 14:46:37 +0530

    Added route to handle request from api gateway middleware

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ app.use(express.json()); //Helps to parse the incoming request body as json
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", apiRoutes);
+app.use("/bookingservice/api", apiRoutes);
 app.listen(ServerConfig.PORT, () => {
   console.log(`Server started at PORT: ${ServerConfig.PORT}`);
   Logger.info("Server up and running.");
